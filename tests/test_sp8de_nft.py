eca6ef450e9121851669cdf339b7d8385dd6c0a7
Mikhail Campos Guadamuz <Mikhail_Campos-Guadamuz@epam.com>
2022-01-14 22:47:10 +0300

    Adding tests for nft logic

---
+++ tests/test_sp8de_nft.py
@@ -0,0 +1,57 @@
+import unittest
+
+import os
+import sys
+import string
+import secrets
+import requests
+import json
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)),'..'))
+from quantralib.erandom import EOSSP8DE_NFT
+
+# ===
+# Adjust to your values
+NFT_CONTRACT = os.environ.get('TEST_NFT_CONTRACT', 'nft_contract')
+NFT_PKEY = os.environ.get('TEST_NFT_PKEY', 'your_private_key')
+NFT_CHAIN = os.environ.get('TEST_NFT_CHAIN', 'http://123.123.123.123:4444')
+NFT_ACCOUNT = os.environ.get('TEST_NFT_ACCOUNT', 'tester')
+# ===
+
+class TestNFT(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+
+        cls.q = EOSSP8DE_NFT(contract_account=NFT_CONTRACT,
+                             p_key=NFT_PKEY,
+                             chain_url=NFT_CHAIN,
+                             chain_port=None)
+
+        # contains forbidden digits for some author
+        alphabet = string.ascii_lowercase + string.digits[7:]
+        cls.invalid_author = '9'+''.join(secrets.choice(alphabet) for i in range(10))
+
+    def test_authorreg_existing_user(self):
+        with self.assertRaises(requests.exceptions.HTTPError) as cm:
+            r = TestNFT.q.authorreg(NFT_ACCOUNT,
+                                    NFT_ACCOUNT,
+                                    "{'info': 'app info'}",
+                                    fieldtypes="type",
+                                    priorityimg="http://unknown.irh/a/b/c/image.png")
+
+        resp = cm.exception.response.json()
+        self.assertEqual(resp['code'], 500)
+        self.assertIn('already registered', resp['error']['details'][0]['message'])
+
+
+    def test_authorreg_invalid_user(self):
+        with self.assertRaises(requests.exceptions.HTTPError) as cm:
+            r = TestNFT.q.authorreg(NFT_ACCOUNT,
+                                    TestNFT.invalid_author,
+                                    "{'info': 'app info'}",
+                                    fieldtypes="type",
+                                    priorityimg="http://unknown.irh/a/b/c/image.png")
+
+        resp = cm.exception.response.json()
+        self.assertEqual(resp['code'], 500)
+        self.assertIn('Name contains invalid character', resp['error']['details'][0]['message'])
