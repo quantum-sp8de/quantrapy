9b8d00b43f3e9a54b549d4527a0d685b3d5f7530
Alexander Dedkov <sanya.dedckov@yandex.ru>
2024-10-17 22:08:21 +0000

    checked and fixed if need it. Methods: get, create, push

--- quantralib/command_line.py
+++ quantralib/command_line.py
@@ -2,6 +2,7 @@ import argparse
 from .cleos import Cleos
 from .testeos import TestEos
 from .utils import parse_key_file, str2bool
+from .keys import EOSKey, Signer
 from .exceptions import InvalidPermissionFormat, EOSSetSameAbi, EOSSetSameCode
 import json
 
@@ -26,38 +27,56 @@ def set_code(ce, account, permission, code, key, broadcast, timeout):
         print('Skipping set code because the new code is the same as the existing code')
 
 
+def parse_keys(private_keys):
+    if not isinstance(private_keys, list):
+        if not isinstance(private_keys, Signer):
+            return [EOSKey(private_keys)]
+    
+    new_priv_keys = []
+    for priv_key in private_keys:
+        if not isinstance(priv_key, Signer):
+                new_priv_keys.append(EOSKey(priv_key))
+    return new_priv_keys
+
 def cleos():
     parser = argparse.ArgumentParser(description='Command Line Interface to EOSIO via python')
     parser.add_argument('--api-version', '-v', type=str, default='v1', action='store', dest='api_version')
-    parser.add_argument('--url', '-u', type=str, action='store', default='https://proxy.eosnode.tools', dest='url')
+    parser.add_argument('--url', '-u', type=str, action='store', default='http://127.0.0.1:8888', dest='url')
     parser.add_argument('--time-out', type=int, action='store', default=30, dest='timeout')
     subparsers = parser.add_subparsers(dest='subparser')
+    
     # get
     get_parser = subparsers.add_parser('get')
     get_subparsers = get_parser.add_subparsers(dest='get')
-    # info
+    
+    # get info
     get_subparsers.add_parser('info')
-    # block
+    
+    # get block
     block_parser = get_subparsers.add_parser('block')
     #block_parser.add_argument('block', type=str)
     block_parser.add_argument('--block', '-b', type=str, action='store', required=True, dest='block')
-    # account
+    
+    # get account
     account_parser = get_subparsers.add_parser('account')
     account_parser.add_argument('--account', '-a', type=str, action='store', required=True, dest='account')
     #account_parser.add_argument('account', type=str)
-    # code
+    
+    # get code
     code_parser = get_subparsers.add_parser('code')
     code_parser.add_argument('--account', '-a', type=str, action='store', required=True, dest='account')
     #code_parser.add_argument('account', type=str)
-    # abi
+    
+    # get abi
     abi_parser = get_subparsers.add_parser('abi')
     abi_parser.add_argument('--account', '-a', type=str, action='store', required=True, dest='account')
     abi_parser.add_argument('--raw', action='store_true', dest='raw')
     #abi_parser.add_argument('account', type=str)
-    # table
+    
+    # get table
     table_parser = get_subparsers.add_parser('table')
     table_parser.add_argument('--code', '-c', type=str, action='store', required=True, dest='code')
-    table_parser.add_argument('--scope', '-S', type=str, action='store', required=True, dest='scope')
+    table_parser.add_argument('--scope', '-s', type=str, action='store', required=True, dest='scope')
     table_parser.add_argument('--table', '-t', type=str, action='store', required=True, dest='table')
     #table_parser.add_argument('contract', type=str, help='The contract who owns the table (required)')
     #table_parser.add_argument('scope', type=str, help='The scope within the contract in which the table is found (required)')
@@ -67,29 +86,35 @@ def cleos():
     table_parser.add_argument('--lower-bound', type=str, action='store', default=0, dest='lower_bound', help='The name of the key to index by as defined by the abi, defaults to primary key')
     table_parser.add_argument('--upper-bound', type=str, action='store', default=-1, dest='upper_bound')
     table_parser.add_argument('--limit', type=int, action='store', default=1000, dest='limit')
-    # currency
+    
+    # get currency
     currency = get_subparsers.add_parser('currency')
     currency.add_argument('type', choices=['balance', 'stats'], type=str)
     currency.add_argument('--code', '-c', type=str, action='store', required=True, dest='code')
     currency.add_argument('--symbol', '-s', type=str, action='store', required=True, dest='symbol')
     currency.add_argument('--account', '-a', type=str, action='store', dest='account')
-    # accounts
-    accounts = get_subparsers.add_parser('accounts')
-    accounts.add_argument('--key', '-k', type=str, action='store', required=True, dest='key')
-    # transaction
+    
+    # # get accounts
+    # accounts = get_subparsers.add_parser('accounts')
+    # accounts.add_argument('--key', '-k', type=str, action='store', required=True, dest='key')
+    
+    # get transaction
     transaction = get_subparsers.add_parser('transaction')
     transaction.add_argument('--transaction', '-t', type=str, action='store', required=True, dest='transaction')
-    # actions
-    actions = get_subparsers.add_parser('actions')
-    actions.add_argument('--account', '-a', type=str, action='store', required=True, dest='account')
-    actions.add_argument('--pos', type=int, action='store', default=-1, dest='pos')
-    actions.add_argument('--offset', type=int, action='store', default=-20, dest='offset')
-    # bin2json
-    bin_json = get_subparsers.add_parser('bin2json')
-    bin_json.add_argument('--code', '-c', type=str, action='store', required=True, dest='code')
-    bin_json.add_argument('--action', '-a', type=str, action='store', required=True, dest='action')
-    bin_json.add_argument('--binargs', '-b', type=str, action='store', required=True, dest='binargs')
-    # json2bin
+    
+    # # get actions
+    # actions = get_subparsers.add_parser('actions')
+    # actions.add_argument('--account', '-a', type=str, action='store', required=True, dest='account')
+    # actions.add_argument('--pos', type=int, action='store', default=-1, dest='pos')
+    # actions.add_argument('--offset', type=int, action='store', default=-20, dest='offset')
+    
+    # # get bin2json
+    # bin_json = get_subparsers.add_parser('bin2json')
+    # bin_json.add_argument('--code', '-c', type=str, action='store', required=True, dest='code')
+    # bin_json.add_argument('--action', '-a', type=str, action='store', required=True, dest='action')
+    # bin_json.add_argument('--binargs', '-b', type=str, action='store', required=True, dest='binargs')
+    # # json2bin
+    
     # create
     create_parser = subparsers.add_parser('create')
     create_subparsers = create_parser.add_subparsers(dest='create')
@@ -98,6 +123,7 @@ def cleos():
     group_key = create_key.add_mutually_exclusive_group(required=True)
     group_key.add_argument('--key-file', '-k', type=str, action='store', help='file to output the keys too', dest='key_file')
     group_key.add_argument('--to-console', '-c', action='store_true', help='output to the console', dest='to_console')
+    
     # push
     push_parser = subparsers.add_parser('push')
     push_subparsers = push_parser.add_subparsers(dest='push')
@@ -108,6 +134,7 @@ def cleos():
     push_action.add_argument('--key-file', '-k', type=str, action='store', required=True, help='file containing the private key that will be used', dest='key_file')
     push_action.add_argument('--permission', '-p', type=str, action='store', required=True, help='account and permission level to use, e.g \'account@permission\'', dest='permission')
     push_action.add_argument('--dont-broadcast', '-d', action='store_false', default=True, help='do not broadcast the transaction to the network.', dest='broadcast')
+    
     # multisig
     msig_parser = subparsers.add_parser('multisig')
     msig_subparsers = msig_parser.add_subparsers(dest='multisig')
@@ -128,8 +155,8 @@ def cleos():
     newacct_parser.add_argument('account', type=str, action='store')
     newacct_parser.add_argument('owner', type=str, action='store')
     newacct_parser.add_argument('--active', '-a', type=str, action='store', dest='active')
-    newacct_parser.add_argument('--stake-net', type=str, action='store', default='1.0000 EOS', dest='stake_net')
-    newacct_parser.add_argument('--stake-cpu', type=str, action='store', default='1.0000 EOS', dest='stake_cpu')
+    newacct_parser.add_argument('--stake-net', type=str, action='store', default='1.0000 SPX', dest='stake_net')
+    newacct_parser.add_argument('--stake-cpu', type=str, action='store', default='1.0000 SPX', dest='stake_cpu')
     newacct_parser.add_argument('--buy-ram-kbytes', type=int, action='store', default=8, dest='ramkb')
     newacct_parser.add_argument('--permission', '-p', type=str, action='store', default='active', dest='permission')
     newacct_parser.add_argument('--transfer', action='store_true', default=False, dest='transfer')
@@ -173,10 +200,10 @@ def cleos():
     buy_random = random_subparsers.add_parser('buyrandom')
     buy_random.add_argument('account', type=str, action='store', help='account name to buy a random value for')
     buy_random.add_argument('--key-file', '-k', type=str, action='store', required=True, help='file containing the private key that will be used', dest='key_file')
-    # register generator
-    reg_generator = random_subparsers.add_parser('reggenerator')
-    reg_generator.add_argument('account', type=str, action='store', help='account name to buy a random value for')
-    reg_generator.add_argument('public_key', type=str, action='store', help='public key for validate random value signature')
+    # # register generator
+    # reg_generator = random_subparsers.add_parser('reggenerator')
+    # reg_generator.add_argument('account', type=str, action='store', help='account name to buy a random value for')
+    # reg_generator.add_argument('public_key', type=str, action='store', help='public key for validate random value signature')
     # nft command
     nft_parser = subparsers.add_parser('nft')
     nft_parser.add_argument('--contract_account', '-c', type=str, action='store', help='account with NFT contract for actions', default="simpleassets")
@@ -311,18 +338,19 @@ def cleos():
                     raise ValueError('--account is required')
             else:
                 console_print(ce.get_currency(code=args.code, symbol=args.symbol, timeout=args.timeout))
-        elif args.get == 'accounts':
-            console_print(ce.get_accounts(args.key, timeout=args.timeout))
+        # elif args.get == 'accounts':
+        #     console_print(ce.get_accounts(args.key, timeout=args.timeout))
         elif args.get == 'transaction':
             console_print(ce.get_transaction(args.transaction, timeout=args.timeout))
-        elif args.get == 'actions':
-            console_print(ce.get_actions(args.account, pos=args.pos, offset=args.offset, timeout=args.timeout))
-        elif args.get == 'bin2json':
-            console_print(ce.abi_bin_to_json(args.code, args.action, args.binargs, timeout=args.timeout))
+        # elif args.get == 'actions':
+        #     console_print(ce.get_actions(args.account, pos=args.pos, offset=args.offset, timeout=args.timeout))
+        # elif args.get == 'bin2json':
+        #     console_print(ce.abi_bin_to_json(args.code, args.action, args.binargs, timeout=args.timeout))  
     # PUSH
     elif args.subparser == 'push':
         if args.push == 'action':
-            priv_key = parse_key_file(args.key_file)
+            priv_keys = parse_keys(parse_key_file(args.key_file))
+
             arguments = json.loads(args.data)
             try:
                 account, permission = args.permission.split('@')
@@ -341,7 +369,7 @@ def cleos():
             payload['data'] = data['binargs']
             print(payload)
             trx = {"actions": [payload]}
-            resp = ce.push_transaction(trx, priv_key, broadcast=args.broadcast)
+            resp = ce.push_transaction(trx, priv_keys, broadcast=args.broadcast)
             console_print(resp)
     # MULISIG
     elif args.subparser == "multisig":
@@ -402,8 +430,8 @@ def cleos():
             console_print(chain.get_randresult(account=args.account))
         if args.random == 'buyrandom':
             console_print(chain.buy_random(account=args.account))
-        if args.random == 'reggenerator':
-            console_print(chain.register_as_generator(account=args.account, public_key=args.public_key))
+        # if args.random == 'reggenerator':
+        #     console_print(chain.register_as_generator(account=args.account, public_key=args.public_key))
     # NFT
     elif args.subparser == 'nft':
         from .spade_nft import EOSSP8DE_NFT
